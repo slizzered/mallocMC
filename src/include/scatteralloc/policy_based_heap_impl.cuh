62ae6d73e804a0bba5de7cb75832efaccd453781
Carlchristian Eckert <c.eckert@hzdr.de>
2014-03-21 11:18:51 +0100

    First try at splitting scatteralloc into policies
     - some smaller scoping problems
     - initHeap needs to be made into a policy

---
+++ src/include/scatteralloc/policy_based_heap_impl.cuh
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "policy_based_heap.cuh"
+
+// global object
+typedef GPUTools::PolicyAllocator< GPUTools::ScatteredHeap<SCATTERALLOC_HEAPARGS>, GPUTools::XMallocDistribution<SCATTERALLOC_HEAPARGS>, NullOnOOM, GPUTools::GetHeapSimpleMalloc > scatterAllocator_T;
+
+__device__ scatterAllocator_T scatterAllocator;
+
+// global initHeap
+void* initHeap(size_t heapsize = 4U*1024U*1024U){
+  void* pool;
+  scatterAllocator_T* heap;
+  SCATTERALLOC_CUDA_CHECKED_CALL(cudaGetSymbolAddress((void**)&heap,scatterAllocator));
+  SCATTERALLOC_CUDA_CHECKED_CALL(cudaMalloc(&pool, heapsize));
+  GPUTools::initKernel<<<1,256>>>(heap,pool, heapsize);
+  return pool;
+};
+
+#ifdef __CUDACC__
+#if __CUDA_ARCH__ >= 200
+// global overwrite malloc/free
+__device__ void* malloc(size_t t) __THROW
+{
+  return scatterAllocator.alloc(t);
+};
+__device__ void  free(void* p) __THROW
+{
+  scatterAllocator.dealloc(p);
+};
+#endif
+#endif
+
+//TODO: globally overwrite new, new[], delete, delete[], placment new, placement new[]
